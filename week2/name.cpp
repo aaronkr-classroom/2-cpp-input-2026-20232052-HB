6fa446a5da7b6549be2827beeeddfa492058c7d9
ththfql <20232052@edu.hanbat.ac.kr>
2026-03-11 17:28:11 +0900

    .

---
+++ week2/name.cpp
@@ -0,0 +1,36 @@
+//name.cpp
+#include <iostream>
+#include <string>
+int main(void) {
+	//이름을 물음
+	std::cout << "First name : >> ";
+	//이름을 읽어들임
+	std::string name;
+	std::cin >> name;
+
+	//출력하려는 메세지를 구성
+	const std::string greeting = "Hello , " + name + "!";
+	/*
+	****************** ==> 1st
+	*                * ==> 2st
+	*  Hello Aaron!  * ==> 3rd
+    *                * ==> 4th
+	****************** ==> 5th
+	*/
+	// 인사말의 2번째 행과 4번째 행
+	const std::string spaces(greeting.size(),' ');
+	const std::string second = "* " + spaces + " *";
+	// 인사말의 첫번째 행과 5번째 행
+	const std::string first(second.size(), '*');
+	// 모두출력 
+	std::cout
+		<< std::endl //줄 바꿈
+		<< first << std::endl
+		<< second << std::endl
+		<< "* " << greeting << " *" << std::endl
+		<< second << std::endl
+		<< first << std::endl
+		<< std::endl;
+
+ 	return 0;
+}
